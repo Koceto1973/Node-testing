659d85029e9ec05f32d9ec4b723630462891127d
TeamK <koceto1973@yahoo.ca>
2019-01-03 00:18:33 +0200

    grouping tests with describe

--- tests/mocha.test.js
+++ tests/mocha.test.js
@@ -1,19 +1,24 @@
 const utilities = require('../utilities');
 
-// adding new test
-it('should add two numbers', () => {
-  var result = utilities.add(33, 11);
+describe('Utils', () => {
+  // adding new test
+  it('should add two numbers', () => {
+    var result = utilities.add(33, 11);
 
-  if (result !== 44) {
-    throw new Error(`Expected 44, but got ${result}.`)
-  }
+    if (result !== 44) {
+      throw new Error(`Expected 44, but got ${result}.`)
+    }
+  });
+
+  // adding new test
+  it('should square a number', () => {
+    var result = utilities.square(3);
+
+    if (result !== 9) {
+      throw new Error(`Expected 9, but got ${result}.`);
+    }
+  });
+    
 });
 
-// adding new test
-it('should square a number', () => {
-  var result = utilities.square(3);
 
-  if (result !== 9) {
-    throw new Error(`Expected 9, but got ${result}.`);
-  }
-});
